51822bdd53f0d34f8293c0cbcdef844c3fd2e0ce
febinsathar <febin.sathar@betasense.io>
2016-09-18 18:09:44 +0530

    change

--- frontend/src/app/utils/viewportDimensions.js
+++ frontend/src/app/utils/viewportDimensions.js
@@ -1,7 +1,6 @@
 const element = document.documentElement;
 const $body = document.getElementsByTagName("body")[0];
 
-// http://tinyurl.com/phansux
 function viewportDimensions() {
     return {
         width: window.innerWidth || element.clientWidth || $body.clientWidth,
